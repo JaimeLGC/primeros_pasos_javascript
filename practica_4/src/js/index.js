90a896766aa704c551a6dc87a273df4c0f2d3639
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-06-02 19:18:50 +0100

    plantilla 4

---
+++ practica_4/src/js/index.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    detectColor();
+}
+
+function detectColor() {
+    var buttons = document.querySelectorAll('#container div');
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        button.addEventListener('click', function() {
+            changeBgColor(this.id);
+        })
+    }
+}
+
+function changeBgColor(color) {
+    document.getElementsByTagName('body')[0].style.backgroundColor = color;
+}
